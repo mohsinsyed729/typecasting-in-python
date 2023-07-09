941ccde063d21ac4ef2378f59f2d38a589b6ce76
mohsinsyed729 <136225413+mohsinsyed729@users.noreply.github.com>
2023-07-09 20:34:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+a=2
+b=3
+print(a+b)
+
+#if values are in quotes (in the form of string)
+a="2"
+b="3"
+#to convert string into number, use int for both
+print(int(a) +int(b))
+
+
+#number and string can also be printed
+c=7
+d="15"
+print(c+ int(d)) 
